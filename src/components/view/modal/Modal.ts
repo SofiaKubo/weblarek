cfbc369175246e977fa07b02e7bc9642d5479f59
SofiaKubo <skuboreva@gmail.com>
2026-02-07 18:43:27 +0100

    chore: add modal view component for close handling

---
+++ src/components/view/modal/Modal.ts
@@ -0,0 +1,47 @@
+import { Component } from '../../base/Component';
+import { ensureElement } from '../../../utils/utils';
+import { IModal, IModalActions } from './types';
+
+export class Modal extends Component<IModal> {
+  protected closeButton: HTMLButtonElement;
+  protected contentElement: HTMLElement;
+
+  constructor(container: HTMLElement, actions: IModalActions) {
+    super(container);
+
+    this.closeButton = ensureElement<HTMLButtonElement>(
+      '.modal__close',
+      this.container
+    );
+
+    this.contentElement = ensureElement<HTMLElement>(
+      '.modal__content',
+      this.container
+    );
+
+    this.closeButton.addEventListener('click', actions.onClose);
+
+    this.container.addEventListener('click', (evt) => {
+      if (evt.target === this.container) {
+        actions.onClose();
+      }
+    });
+  }
+
+  set content(value: HTMLElement | null) {
+    this.contentElement.replaceChildren();
+
+    if (value) {
+      this.contentElement.append(value);
+    }
+  }
+
+  open(): void {
+    this.container.classList.add('modal_active');
+  }
+
+  close(): void {
+    this.container.classList.remove('modal_active');
+    this.content = null;
+  }
+}
