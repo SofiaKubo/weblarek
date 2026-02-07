f0c30adfa7b81928e16dcfcf6d9b14aaa1b5e70a
SofiaKubo <skuboreva@gmail.com>
2026-02-07 20:24:13 +0100

    refactor: reuse container for gallery items

--- src/components/view/gallery/Gallery.ts
+++ src/components/view/gallery/Gallery.ts
@@ -2,15 +2,11 @@ import { Component } from '../../base/Component';
 import { IGallery } from './types';
 
 export class Gallery extends Component<IGallery> {
-  protected catalogElement: HTMLElement;
-
   constructor(container: HTMLElement) {
     super(container);
-
-    this.catalogElement = container;
   }
 
   set catalog(items: HTMLElement[]) {
-    this.catalogElement.replaceChildren(...items);
+    this.container.replaceChildren(...items);
   }
 }
