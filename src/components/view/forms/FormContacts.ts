9bce63ae6613968a65fa0fbdab2cf1e9e90dcf80
SofiaKubo <skuboreva@gmail.com>
2026-02-08 19:46:11 +0100

    refactor: add accessors for contacts form inputs

--- src/components/view/forms/FormContacts.ts
+++ src/components/view/forms/FormContacts.ts
@@ -1,8 +1,30 @@
 import type { IFormActions } from './types';
 import { FormBase } from './FormBase';
+import { ensureElement } from '../../../utils/utils';
 
 export class FormContacts extends FormBase {
+  private readonly emailInput: HTMLInputElement;
+  private readonly phoneInput: HTMLInputElement;
+
   constructor(container: HTMLFormElement, actions: IFormActions) {
     super(container, actions);
+
+    this.emailInput = ensureElement<HTMLInputElement>(
+      'input[name="email"]',
+      this.container
+    );
+
+    this.phoneInput = ensureElement<HTMLInputElement>(
+      'input[name="phone"]',
+      this.container
+    );
+  }
+
+  set email(value: string) {
+    this.emailInput.value = value;
+  }
+
+  set phone(value: string) {
+    this.phoneInput.value = value;
   }
 }
